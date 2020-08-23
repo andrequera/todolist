36c01ba8332f065d72bb50139a21f7076714d649
andrequera <andrequera@gmail.com>
2020-08-23 16:41:32 -0400

    listadetarea

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [state, setState] = useState([])
+  // [] guardo los valores ingresados a mi lista que empieza sin nada.
+  const valor = (capturaevento) => {
+    if (capturaevento.keyCode === 13) {
+      //key13 es para que se mande toda la palabra con el enter 
+      // para evitar que se mande el formulario preventDefault
+      capturaevento.preventDefault();
+      // archivando mis valores en el state ... sprit operator
+      setState([...state, capturaevento.target.value])
+
+      // para borrar el input, comillas en blannnco
+      capturaevento.target.value = "";
+    }
+
+    //toma todo los cambios ingresados en mi cuadrito input
+  }
+  //console.log(state);
+  
+  const borra=(i)=>{
+    //haciendo borrar, colocar [] porque usa es arreglo
+    state.splice(i,1);
+    setState([...state]);
+    //console.log(state, i);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>LISTA DE TAREAS</p>
+        <input className="form-control form-control-lg" type="text" placeholder="Lista de tarea"
+          onKeyDown={(capturaevento) => valor(capturaevento)} />
+        {/* mezcla de html con js llaves */}
+        {/* html ... para escribir dentro de html en el js uso llaves */}
+        <ul>
+          {
+            //lista de mis actividades
+            state.map((l, i) => {
+              return (
+                <li key={i} >{l} <img onClick={()=>borra(i)} src="https://laletra.org/wp-content/uploads/2019/10/letters-2077251_960_720.png" width="30" alt=""/> </li>
+              )
+            })
+
+          }
+        </ul>
       </header>
     </div>
   );
